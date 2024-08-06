16aa0b0cff562461cd322eaac95ed5f8b572c1dd
Jonathan Padin <padinjonathan1@gmail.com>
2024-08-06 18:45:10 -0300

    Correciones en la pagina de contacto, validaciones agregadas

--- contacto.js
+++ contacto.js
@@ -1,28 +1,71 @@
-
-let nombre = document.getElementById('nombre');
-let apellido = document.getElementById('apellido');
-let email = document.getElementById('email');
-let telefono = document.getElementById('telefono');
-let mensaje = document.getElementById('mensaje');
-let btnEnviar = document.getElementById('enviar');
+const nombre = document.getElementById('nombre');
+const apellido = document.getElementById('apellido');
+const email = document.getElementById('email');
+const telefono = document.getElementById('telefono');
+const mensaje = document.getElementById('mensaje');
+const btnEnviar = document.getElementById('enviar');
+const check = document.getElementById('check');
 
 let informacion = [];
 
-btnEnviar.addEventListener("click", (e)=>{
-    e.preventDefault(); //Previene la accion del form de actualizar la pag
+btnEnviar.addEventListener("click", (e) => {
+    e.preventDefault();
 
-    informacion[0] = nombre.value;
-    informacion[1] = apellido.value;
-    informacion[2] = email.value;
-    informacion[3] = telefono.value;
-    informacion[4] = mensaje.value;
+    let valorValido = false;
+    let errores = "";
 
-    let blob = new Blob ([informacion], {type: "text/plain;charset=utf-8"});
+    if (nombre.value.length < 3) {
+        errores += "Error de longitud del nombre, ingrese un nombre correcto. ";
+        valorValido = true;
+    }
 
-    saveAs(blob, "contacto.txt");
+    if (apellido.value.length < 3) {
+        errores += "Error de longitud de apellido, ingrese un apellido correcto. ";
+        valorValido = true;
+    }
 
-    let check = document.getElementById('check')
-    check.classList.add('confirma')
-    check.innerText = `¡Mensaje enviado con exito!`;
-})
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (!regexEmail.test(email.value)) {
+        errores += "Error de email, por favor ingrese un email correcto.";
+        valorValido = true;
+    }
+
+    let regexTelefono = /^[0-9]{6,10}$/;
+
+    if (!regexTelefono.test(telefono.value)) {
+        errores += "Telefono invalido, debe contener entre 6 y 10 numeros. ";
+        valorValido = true;
+    }
+
+    if (mensaje.value.length < 20) {
+        errores += "Error de longitud del mensaje, necesitas mas de 50 caracteres.";
+        valorValido = true;
+    }
+
+    if (valorValido) {
+        check.classList.add('error');
+        check.innerText = errores;
+    } else {
+        informacion = [
+            nombre.value,
+            apellido.value,
+            email.value,
+            telefono.value,
+            mensaje.value
+        ];
+
+        let blob = new Blob([informacion], {type: "text/plain;charset=utf-8"});
+        saveAs(blob, "contacto.txt");
+
+        check.classList.remove('error');
+        check.classList.add('confirma');
+        check.innerText = "¡Mensaje enviado con éxito!";
+        nombre.value = "";
+        apellido.value = "";
+        email.value = "";
+        telefono.value = "";
+        mensaje.value = "";
+    }
+});
+
 
--- productos.js
+++ productos.js
@@ -1,106 +1,106 @@
 let productos = [
-  { 
+  {
     nombre: "Leche",
     precio: 1500,
     stock: 6,
-    img : "leche.png",
+    img: "leche.png",
   },
-  { 
+  {
     nombre: "Quesos",
     precio: 1700,
     stock: 5,
-    img : "queso.png",
+    img: "queso.png",
   },
-  { 
+  {
     nombre: "Verduras",
     precio: 1200,
     stock: 12,
-    img : "verduras.png",
+    img: "verduras.png",
   },
-  { 
+  {
     nombre: "Carnes",
     precio: 1900,
     stock: 13,
-    img : "carnes.png", 
+    img: "carnes.png",
   },
-  { 
+  {
     nombre: "Pastas",
     precio: 1000,
     stock: 9,
-    img : "pastasproducto.png",
+    img: "pastasproducto.png",
   },
-  { 
+  {
     nombre: "Arroz",
     precio: 1150,
     stock: 10,
-    img : "arroz.png", 
+    img: "arroz.png",
   },
-  { 
+  {
     nombre: "Azúcar",
     precio: 1400,
     stock: 8,
-    img : "azucar.png",
+    img: "azucar.png",
   },
-  { 
+  {
     nombre: "Harina",
     precio: 1250,
     stock: 6,
-    img : "harina.png",
+    img: "harina.png",
   },
-  { 
+  {
     nombre: "Sal",
-    precio: 500, 
+    precio: 500,
     stock: 12,
-    img : "sal.png", 
+    img: "sal.png",
   },
-  { 
+  {
     nombre: "Aceite",
     precio: 1180,
     stock: 5,
-    img : "aceite.png",
-  }
+    img: "aceite.png",
+  },
 ];
 
-let cantidadComprada = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let cantidadComprada = [];
 
-const listaProductos = document.getElementById('listaProductos');
+const listaProductos = document.getElementById("listaProductos");
 //Creacion de la lista de compra,los productos, su imagen, su stock, etc
 for (let i = 0; i < productos.length; i++) {
   let producto = productos[i];
 
-  let item = document.createElement('div');
-  item.classList.add('producto');
+  let item = document.createElement("div");
+  item.classList.add("producto");
 
-  let imgDescuento = document.createElement('img');
-  imgDescuento.classList.add('imgDescuento');
-  imgDescuento.src = 'oferta.png';
-
-  let img = document.createElement('img');
-  img.classList.add('imgProductos');
-  img.src = producto.img;
-  img.alt = producto.nombre;
-
-  let parrafos = document.createElement('div');
-  parrafos.classList.add('parrafos');
-
-  let nombre = document.createElement('p');
-  nombre.innerText = `${producto.nombre}: El precio es de $${producto.precio}`;
-
-  let stock = document.createElement('p');
-  stock.id = `stock-${i}`;
-  stock.innerText = `Stock actual: ${producto.stock}`;
+  let imgDescuento = document.createElement("img");
+  imgDescuento.classList.add("imgDescuento");
+  imgDescuento.src = "oferta.png";
 
   if(producto.stock >= 10){
     imgDescuento.style.display = 'inline';
   }
 
-  let input = document.createElement('input');
+  let img = document.createElement("img");
+  img.classList.add("imgProductos");
+  img.src = producto.img;
+  img.alt = producto.nombre;
+
+  let parrafos = document.createElement("div");
+  parrafos.classList.add("parrafos");
+
+  let nombre = document.createElement("p");
+  nombre.innerText = `${producto.nombre}: El precio es de $${producto.precio}`;
+
+  let stock = document.createElement("p");
+  stock.id = `stock-${i}`;
+  stock.innerText = `Stock actual: ${producto.stock}`;
+
+  let input = document.createElement("input");
   input.classList.add('caja');
-  input.type = 'number';
-  input.min = '0';
+  input.type = "number";
+  input.min = "0";
   input.max = producto.stock.toString();
-  input.value = '0';
-  input.addEventListener('change', () => {
+  input.value = "0";
+  input.addEventListener("change", () => {
     cantidadComprada[i] = input.value;
   });
 
@@ -113,8 +113,6 @@ for (let i = 0; i < productos.length; i++) {
   listaProductos.appendChild(item);
 }
 
-
-// Función para calcular el total de la compra
 function calcularTotal() {
   let total = 0;
   for (let i = 0; i < cantidadComprada.length; i++) {
@@ -123,30 +121,35 @@ function calcularTotal() {
       total += cantidad * productos[i].precio;
     }
   }
-  document.getElementById('total').innerText = total;
+  document.getElementById("total").innerText = total;
 }
 
-// Evento para el botón de calcular total
-document.getElementById('calcularTotal').addEventListener('click', calcularTotal);
+document
+  .getElementById("calcularTotal")
+  .addEventListener("click", calcularTotal);
 
-// Evento para el botón de comprar
-document.getElementById('comprar').addEventListener('click', () => {
+document.getElementById("comprar").addEventListener("click", () => {
   calcularTotal();
-  let total = document.getElementById('total').innerText;
+  let total = document.getElementById("total").innerText;
+  let check = document.getElementById("check");
   if (total > 0) {
-    let check = document.getElementById('check');
-    check.classList.add('confirma');
+    check.classList.remove("rechaza");
+    check.classList.add("confirma");
     check.innerText = `¡Compra realizada con éxito! El total de tu compra es de: $${total}`;
-    for(let i = 0; i < productos.length; i++) {
-      if (cantidadComprada[i] > 0 && cantidadComprada[i] <= productos[i].stock) {
+    for (let i = 0; i < productos.length; i++) {
+      if (
+        cantidadComprada[i] > 0 &&
+        cantidadComprada[i] <= productos[i].stock
+      ) {
         productos[i].stock -= cantidadComprada[i];
-        document.getElementById(`stock-${i}`).innerText = `Stock actual: ${productos[i].stock}`;
+        document.getElementById(
+          `stock-${i}`
+        ).innerText = `Stock actual: ${productos[i].stock}`;
       }
     }
-  
   } else {
-    let check = document.getElementById('check');
-    check.classList.add('rechaza');
-    check.innerText = "No hay productos seleccionados o la cantidad es cero.";
+    check.classList.remove("confirma");
+    check.classList.add("rechaza");
+    check.innerText = "No hay productos seleccionados, no hay stock o la cantidad es cero.";
   }
 });
